069406ed6fd554eeffd7bdbc04fab37cee3420a3
JamesEditionZz <kanpongpgm@gmail.com>
2026-03-25 00:17:18 +0700

    Update Page

--- frontend/app/components/Header/page.tsx
+++ frontend/app/components/Header/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect } from "react";
+import Header from "../page";
 import Main from "../Main/page";
 import Sub from "../Sub_Main/page";
 import Sub2 from "../Sub_Main2/page";
@@ -62,35 +63,9 @@ export default function page() {
           ))}
           <hr className="text-neutral-500 border-1 mb-3" />
           {Toggle.length === 0 ? (
-            <div className="grid grid-cols-5 gap-4 text-lg p-8 max-[500px]:grid-cols-1 max-[500px]:gap-2 max-[800px]:grid-cols-2 max-[800px]:gap-2 max-[1200px]:grid-cols-3 max-[1200px]:gap-2 max-[1600px]:grid-cols-4 max-[1600px]:gap-2">
-              {dataProductGroup.map((item: any, index: number) => (
-                <div
-                  key={index}
-                  className="cursor-pointer"
-                  onClick={() => {
-                    SelectProduct(item.Product_Type);
-                  }}
-                >
-                  <div>
-                    <Image
-                      className="Images-Group"
-                      src={`/ImageFront/${item.Product_Type}.jpg`}
-                      width={1000}
-                      height={1000}
-                      alt={item.Product_Type}
-                    />
-                  </div>
-                  <div className="text-center text-base/6 p-1 bg-neutral-700 text-white font-normal font-sans">
-                    <div className="font-12">
-                      {item.Product_Name.split("|")[0]}
-                    </div>
-                  </div>
-                  <div className="text-center text-base/6 p-1 bg-neutral-500 text-white font-th font-sans">
-                    {item.Product_Name.split("|")[1]}
-                  </div>
-                </div>
-              ))}
-            </div>
+            <>
+              <Header onUpdate={SelectProduct} />
+            </>
           ) : Toggle.length === 2 ? (
             <>
               <Main ValueToggle={Toggle} onUpdate={SelectProduct} />
@@ -110,7 +85,7 @@ export default function page() {
           ) : (
             Toggle.length === 10 && (
               <>
-                <FinalProduct ValueToggle={Toggle} onUpdate={SelectProduct} />
+                <FinalProduct ValueToggle={Toggle} />
               </>
             )
           )}
